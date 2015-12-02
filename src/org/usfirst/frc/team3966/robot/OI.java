91cf3dd3aa0b32b506fee82c1f7993ce45aacc24
Ben Klein <robobenklein@gmail.com>
2015-12-02 11:54:05 -0500

    Fixing indentations to be consistent

--- src/org/usfirst/frc/team3966/robot/OI.java
+++ src/org/usfirst/frc/team3966/robot/OI.java
@@ -9,50 +9,50 @@ import edu.wpi.first.wpilibj.buttons.Button;
  */
 public class OI {
 
-    //// DEFINITION OF DRIVE LAYOUT
-    // What control style are we using?
-    // We have both Joysticks and Game controllers now at our disposal,
-    // so let's adapt for both just in case.
+  //// DEFINITION OF DRIVE LAYOUT
+  // What control style are we using?
+  // We have both Joysticks and Game controllers now at our disposal,
+  // so let's adapt for both just in case.
 
-    public static boolean controllerEnabled = false;
-    
-    public Joystick rstick;
-    public Joystick lstick;
-    public Joystick fstick;
-    public Joystick controller;
+  public static boolean controllerEnabled = false;
 
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    static {
-      if ( !controllerEnabled ) {
-        Joystick rstick = new Joystick(0); // Right drive stick
-        Joystick lstick = new Joystick(1); // Left drive stick
-        Joystick fstick = new Joystick(2); // Arm and superstructure stick
-      } else if ( controllerEnabled ) {
-        Joystick controller = new Joystick(0); // A single controller for Shrimp
-      }
+  public Joystick rstick;
+  public Joystick lstick;
+  public Joystick fstick;
+  public Joystick controller;
+
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  static {
+    if ( !controllerEnabled ) {
+      Joystick rstick = new Joystick(0); // Right drive stick
+      Joystick lstick = new Joystick(1); // Left drive stick
+      Joystick fstick = new Joystick(2); // Arm and superstructure stick
+    } else if ( controllerEnabled ) {
+      Joystick controller = new Joystick(0); // A single controller for Shrimp
     }
-    // Button button = new JoystickButton(stick, buttonNumber);
+  }
+  // Button button = new JoystickButton(stick, buttonNumber);
 
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
+  // There are a few additional built in buttons you can use. Additionally,
+  // by subclassing Button you can create custom triggers and bind those to
+  // commands the same as any other Button.
 
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
 
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new TankDrive());
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new TankDrive());
 
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new TankDrive());
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new TankDrive());
 
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new TankDrive());
+  // Start the command when the button is released  and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new TankDrive());
 }
--- src/org/usfirst/frc/team3966/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team3966/robot/subsystems/Drive.java
@@ -9,29 +9,33 @@ import org.usfirst.frc.team3966.robot.commands.TankDrive;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
+ * Drive subsystem, controls left and right motors.
  *
+ * Supplies methods for controlling the drive by various functions.
+ *
+ * The motors objects should not be directly set from any other place.
  */
 public class Drive extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
 	Talon rightmotor = new Talon(1); // Right motor #1
 	Talon leftmotor = new Talon(0); // Left motor #2
-	
+
 	public void doNothing() {
 		rightmotor.set(0);
 		leftmotor.set(0);
 	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        setDefaultCommand(new TankDrive());
-    }
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new TankDrive());
+  }
 
 	public void TankDrive(double rightmotorspeed, double leftmotorspeed) {
 		rightmotor.set(rightmotorspeed);
 		leftmotor.set(leftmotorspeed);
-		
+
 	}
 }
