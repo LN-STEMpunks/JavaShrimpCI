656387193bae5ae201fad95135ffaed76e140212
Ben Klein <robobenklein@gmail.com>
2015-12-02 11:41:13 -0500

    Remove uneeded import

--- src/org/usfirst/frc/team3966/robot/OI.java
+++ src/org/usfirst/frc/team3966/robot/OI.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team3966.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
-import org.usfirst.frc.team3966.robot.commands.TankDrive;
 
 /**
  * This class is the glue that binds the controls on the physical operator
