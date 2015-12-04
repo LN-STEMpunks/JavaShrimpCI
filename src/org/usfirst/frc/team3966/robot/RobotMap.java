c49000b6d61f0675abb2aa59964478d02cf5de2a
mrtimklein <mrtimklein@yahoo.com>
2015-12-03 21:13:52 -0500

    playing with leftmotor classes

--- src/org/usfirst/frc/team3966/robot/RobotMap.java
+++ src/org/usfirst/frc/team3966/robot/RobotMap.java
@@ -13,15 +13,15 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  */
 public class RobotMap {
     // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
-	
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
+
 	public final int rstick = 0;
 	public final int lstick = 1;
 	public final int fstick = 2;
